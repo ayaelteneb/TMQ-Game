03089db529d5a6953cf09da75266da923e3d18fc
ayaelteneb <118103607+ayaelteneb@users.noreply.github.com>
2023-11-30 15:47:42 +0200

    chaining chest

--- Assets/OpenChest.cs
+++ Assets/OpenChest.cs
@@ -4,13 +4,18 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class OpenChest : MonoBehaviour
 {
     public Animator animator;
+    private TreasureControl treasure;
 
-
+    private void Start()
+    {
+        treasure = GetComponent<TreasureControl>();
+    }
 
 
     public void PlayChestMovementAnimation()
     {
         // Set the trigger to play the "chest_opened" animation
         animator.Play("chest_opened");
+        treasure.RevealNextChest();
     }
 }
