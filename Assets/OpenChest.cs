4b1bb2c7cc7e66cb820b3e3abadcdb743694dd83
ayaelteneb <118103607+ayaelteneb@users.noreply.github.com>
2023-11-30 15:34:04 +0200

    ADD CHEST MOVEMENT

--- Assets/OpenChest.cs
+++ Assets/OpenChest.cs
@@ -1,25 +1,16 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class OpenChest : XRGrabInteractable
+public class OpenChest : MonoBehaviour
 {
-    private Animator animator;
+    public Animator animator;
 
-    protected override void Awake()
-    {
-        base.Awake();
-        animator = GetComponentInChildren<Animator>();
-    }
 
-    protected override void OnSelectEntered(SelectEnterEventArgs args)
-    {
-        base.OnSelectEntered(args);
-        PlayChestMovementAnimation();
-    }
 
-    private void PlayChestMovementAnimation()
+
+    public void PlayChestMovementAnimation()
     {
         // Set the trigger to play the "chest_opened" animation
-        animator.SetTrigger("OpenChestTrigger");
+        animator.Play("chest_opened");
     }
 }
