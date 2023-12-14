a858df5db30620f5b73ec8e397b8dcc4ff782996
ayaelteneb <118103607+ayaelteneb@users.noreply.github.com>
2023-12-14 23:13:36 +0200

    ButtonClue7,8

---
+++ Assets/Scripts/openRbutton2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class openRbutton2 : MonoBehaviour
+{
+    public GameObject gameObject;
+    bool active;
+    public void OpenAndClose()
+    {
+        if (active == false)
+        {
+            gameObject.transform.gameObject.SetActive(true);
+            active = true;
+        }
+        else 
+        {
+            gameObject.transform.gameObject.SetActive(false);
+            active = false;
+        }
+    }
+}
